8188360cb6350e53b75df04f2122417156688fe0
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-03-19 17:29:30 +0400

    fix: Extra changes compatible with Spring Boot 3.5.11: Junit is replaced with Jupiter, tests are fixed correspondingly. Bump mockito-core to 5.23.0, maven-compiler-plugin to 3.15.0.

--- src/test/java/org/qubership/atp/crypt/CryptoToolsTest.java
+++ src/test/java/org/qubership/atp/crypt/CryptoToolsTest.java
@@ -1,5 +1,5 @@
 /*
- * # Copyright 2024-2025 NetCracker Technology Corporation
+ * # Copyright 2024-2026 NetCracker Technology Corporation
  * #
  * # Licensed under the Apache License, Version 2.0 (the "License");
  * # you may not use this file except in compliance with the License.
@@ -16,23 +16,26 @@
 
 package org.qubership.atp.crypt;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class CryptoToolsTest {
 
     private static final String UNENCRYPTED_DATA = "Not encrypted text";
     private static final String ENCRYPTED_DATA =
-            "{ENC}{SQlLQ8SeZ5YpsNHL}{SQlLQ8SeZ5YpsNHLZ8Wkf+ku3Lls485XJJDcNX2id6Ym6zwScta" +
-                    "+fGheVULW3o2au1vrWkf08K5mjUs4xibnJRaKv2Fff0RvT5oe2QGYPC9asu2imP5ZMtPQfvkNW9A4AuyDQLtTXCENa0zER33VregFmoromc8jzIP486XnZqkHCxOc3J7wz8+jWW4QDG7Ya7qafWhwuXhPc9J8yPqXn7ASfV4EKhev+1MjDD6n2Es4IXKO9f5UksmWP22yikTij2nqjAu5mYVuW/0OzGTslBJ+FdUH0OmCPsIajjRyzcDIeUkb0GPGsH72cnv3ysx1nPdnFkQyR2jycuX5GcTONQ==}";
+            "{ENC}{SQlLQ8SeZ5YpsNHL}{SQlLQ8SeZ5YpsNHLZ8Wkf+ku3Lls485XJJDcNX2id6Ym6zwScta"
+                    + "+fGheVULW3o2au1vrWkf08K5mjUs4xibnJRaKv2Fff0RvT5oe2QGYPC9asu2imP5ZMtPQfvkNW9A4AuyDQLtTXCENa"
+                    + "0zER33VregFmoromc8jzIP486XnZqkHCxOc3J7wz8+jWW4QDG7Ya7qafWhwuXhPc9J8yPqXn7ASfV4EKhev+1MjDD6"
+                    + "n2Es4IXKO9f5UksmWP22yikTij2nqjAu5mYVuW/0OzGTslBJ+FdUH0OmCPsIajjRyzcDIeUkb0GPGsH72cnv3ysx1n"
+                    + "PdnFkQyR2jycuX5GcTONQ==}";
 
     @Test
     public void maskEncryptedData_whenNullData() {
         String maskedData = CryptoTools.maskEncryptedData(null);
-        assertEquals(null, maskedData);
+        assertNull(maskedData);
     }
-
 
     @Test
     public void maskEncryptedData_whenEmptyData() {
@@ -45,7 +48,6 @@ public class CryptoToolsTest {
         String maskedData = CryptoTools.maskEncryptedData(UNENCRYPTED_DATA);
         assertEquals(UNENCRYPTED_DATA, maskedData);
     }
-
 
     @Test
     public void maskEncryptedData_whenEncrypted() {
--- src/test/java/org/qubership/atp/crypt/config/AtpCryptoConfigurationTest.java
+++ src/test/java/org/qubership/atp/crypt/config/AtpCryptoConfigurationTest.java
@@ -1,5 +1,5 @@
 /*
- * # Copyright 2024-2025 NetCracker Technology Corporation
+ * # Copyright 2024-2026 NetCracker Technology Corporation
  * #
  * # Licensed under the Apache License, Version 2.0 (the "License");
  * # you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 
 package org.qubership.atp.crypt.config;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.qubership.atp.crypt.exception.AtpCryptoException;
 
 public class AtpCryptoConfigurationTest {
